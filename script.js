731bfe00a0bcf1488cd5f4ce952db56f7acb8614
Jitender Kumar <111197506+Jitender72658@users.noreply.github.com>
2023-07-24 12:45:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function calculateMinCost() {
   //your code here
-  
+ let lengths = document.getElementById('rope-lengths');
+ let values = lengths.value.split(',');
+	values.sort();
+	values.forEach((val)=>{
+		console.log(val);
+	})
+	console.log(values);
+ let ans = document.getElementById('result');
+	ans.innerText = values;
   
   
 }  
